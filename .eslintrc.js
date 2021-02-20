2fc2d9c85562836e6a3cceb03bc5b0d54c01ba20
carlosx71 <carlos.x71@hotmail.com>
2021-02-20 17:55:32 -0300

    chore: :tada: Init project

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,11 @@ module.exports = {
     'eslint-plugin-import-helpers',
   ],
   rules: {
+    // Start rules to windows CRLF
+    "linebreak-style": 0,
+    "global-require": 0,
+    "eslint linebreak-style": [0, "error", "windows"],
+    // End rules to windows CRLF
     'react/jsx-filename-extension': [
       'error',
       {
